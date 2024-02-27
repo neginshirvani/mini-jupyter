dcb609a134aa92328c9122aec9d33f6821fef4f0
negin <negin@pop-os.localdomain>
2024-02-27 11:01:12 +0100

    run-py

--- code_executer/views.py
+++ code_executer/views.py
@@ -1,17 +1,38 @@
-from django.shortcuts import render
-
-# Create your views here.
+import io
+import sys
 from rest_framework.views import APIView
-from rest_framework.generics import CreateAPIView
-
 from rest_framework.response import Response
 from rest_framework import status
-import sys
-import io
-
 from .models import Code
 from .serializers import CodeSerializer
 
-class CodeView(CreateAPIView):
-    serializer_class = CodeSerializer
 
+class CodeView(APIView):
+    serializer_class = CodeSerializer
+    queryset = Code.objects.all()
+
+    def post(self, request, *args, **kwargs):
+        serializer = CodeSerializer(data=request.data)
+        if serializer.is_valid():
+            code_obj = serializer.save()
+            code_to_execute = serializer.validated_data['code']
+            print("--------------------------------")
+
+            old_stdout = sys.stdout
+            redirected_output = sys.stdout = io.StringIO()
+
+            try:
+                exec(code_to_execute)
+                exec_output = redirected_output.getvalue()
+                response_data = {
+                    "code": code_to_execute,
+                    "output": exec_output
+                }
+                print("-------------------------------")
+                print("code:", response_data['code'])
+                return Response(response_data, status=status.HTTP_201_CREATED)
+            except Exception as e:
+                return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+            finally:
+                sys.stdout = old_stdout
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
